4a4e7dce328f9b98aa7ab1b1af04ba7a3c38c983
Jennings <1716sjmcrash@gmail.com>
2019-06-10 23:28:45 +0530

    ldap complete

--- React_Tutorial/react_app_Azure_Act_Dir/react_act_dir/src/App.js
+++ React_Tutorial/react_app_Azure_Act_Dir/react_act_dir/src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
+import {authContext, adalGraphFetch } from './adalconfig';
 
-function App() {
-  return (
+
+class App extends Component {
+   state = {
+    apiResponse: ''
+  };
+
+  componentDidMount() {
+
+    // We're using Fetch as the method to be called, and the /me endpoint 
+    // from Microsoft Graph as the REST API request to make.
+    adalGraphFetch(fetch, 'https://graph.microsoft.com/v1.0/me', {})
+      .then((response) => {
+
+        // This is where you deal with your API response. In this case, we            
+        // interpret the response as JSON, and then call `setState` with the
+        // pretty-printed JSON-stringified object.
+        // console.log("response",response.json());
+        response.json()
+          .then((responseJson) => {
+            console.log("response",responseJson);
+            this.setState({ apiResponse: JSON.stringify(responseJson, null, 2) })
+          });
+      })
+      .catch((error) => {
+
+        // Don't forget to handle errors!
+        console.error(error);
+      })
+  }
+
+  logout(){
+    console.log("Logging out");
+    authContext.logOut()
+   }
+  
+   render(){
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hiii</h1>
+      <button onClick={this.logout  }>Logout</button>
     </div>
   );
+  }
+  
 }
-
 export default App;
--- React_Tutorial/react_app_Azure_Act_Dir/react_act_dir/src/adalconfig.js
+++ React_Tutorial/react_app_Azure_Act_Dir/react_act_dir/src/adalconfig.js
@@ -1,15 +1,27 @@
-import { AuthenticationContext } from 'react-adal';
+import { AuthenticationContext,adalFetch,withAdalLogin} from 'react-adal';
 const adalConfig = {
- tenant: 'xxxxx.onmicrosoft.com',
- clientId: '',//client-id
+ tenant: 'jenningsteam.onmicrosoft.com',
+ clientId: 'xxx',//client-id
  endpoints: {
-   api: ''//identitymetdata
+   api: 'https://login.microsoftonline.com/xxx-xxx/.well-known/openid-configuration',//identitymetdata,
+   graphApi: 'https://graph.microsoft.com',
  },
  postLogoutRedirectUri: window.location.origin,
- redirectUri: '',//return URL
- cacheLocation: 'sessionStorage'
+ redirectUri: 'http://localhost:3000/auth/openid/return/',//return URL
+ cacheLocation: 'localStorage'
 };
 export const authContext = new AuthenticationContext(adalConfig);
 export const getToken = () => {
  return authContext.getCachedToken(authContext.config.clientId);
 };
+
+export const adalApiFetch = (fetch, url, options) =>{
+  return adalFetch(authContext, adalConfig.endpoints.api, fetch, url, options);
+
+}
+
+export const adalGraphFetch = (fetch, url, options) =>
+  adalFetch(authContext, adalConfig.endpoints.graphApi, fetch, url, options);
+
+
+export const withAdalLoginApi = withAdalLogin(authContext, adalConfig.endpoints.api);
